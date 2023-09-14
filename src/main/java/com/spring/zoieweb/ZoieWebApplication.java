e8386ede71b0d5710a6e1647d01e68fcc283869f
Yunju Jo <yunjujo61@gmail.com>
2023-09-14 21:48:20 +0900

    Initial commit

---
+++ src/main/java/com/spring/zoieweb/ZoieWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.zoieweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoieWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoieWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/zoieweb/ZoieWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.zoieweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoieWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
